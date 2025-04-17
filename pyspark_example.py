8d4743ea0ac90dfc419cd9313b0ce51a30ec2a2f
Sarachou88 <sarah.ruttiens4@gmail.com>
2025-04-17 13:08:21 +0200

    Create pyspark_example.py

---
+++ pyspark_example.py
@@ -0,0 +1,23 @@
+# Import packages
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+# Retreives the existing session called "InterviewExample" or create a new session if not found
+spark = SparkSession.builder.appName("InterviewExample").getOrCreate()
+
+# Define the data
+data = [
+    ("Alice", 28),
+    ("Bob", 35),
+    ("Cathy", 22)
+]
+
+# Transform the data into a Dataframe
+df = spark.createDataFrame(data, ["name", "age"])
+
+# Filter users older than 25
+filtered_df = df.filter(col("age") > 25)
+
+# Print the output of the filtered table 
+# --> Lazy evaluation (only apply filter if .show() or .collect())
+filtered_df.show()
